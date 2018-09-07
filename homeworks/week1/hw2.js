3b6b775f53cec286684ce0b158bb6a25edc70148
Ting <tingkao4124@gmail.com>
2018-09-07 15:58:13 +0800

    wh2-fixed

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,18 +1,14 @@
 function capitalize(str) {
-    var fst_letter = str[0]
-    var fst_word = str.split(' ')[0]
+    var fst_letter = str[0].toUpperCase()
     var other_words = ''
     var word = ''
-    if('A'<= fst_letter && fst_letter <='z'){
-      for(var i=1; i<fst_word.length ; i++){
-          other_words = other_words + fst_word[i]
-      }
-      word = fst_letter.toUpperCase() + other_words
-      return word
-    }else{
-      return fst_word
+    for(var i=1; i<str.length ; i++){
+      other_words += str[i]
     }
+    word = fst_letter + other_words
+    return word
   }
   
-  console.log(capitalize('hello word!'))
-  console.log(capitalize('!!hey u'))
+  // console.log(capitalize('hello word!'))
+  // console.log(capitalize('!!hey u'))
+  // console.log(capitalize('asdaknsjncwei'))
