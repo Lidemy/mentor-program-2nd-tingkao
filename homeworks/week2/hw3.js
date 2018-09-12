20fd9b2a955e20fba9049b98832fdbf63de279bf
Ting <tingkao4124@gmail.com>
2018-09-12 18:41:09 +0800

    week2-homework

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,18 @@
 function isPrime(n) {
-  
+    var arr = []
+    var isPrime = false
+    for(var i=2; i<n; i++){
+        arr.push(i)
+    }
+    for(var i=2; i<n; i++){
+        if(n % arr[i] === 0){
+            isPrime = false
+            break
+        }else {
+            isPrime = true
+        }
+    }
+    return isPrime
 }
 
 module.exports = isPrime
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,5 +1,38 @@
 function add(a, b) {
-  
+    var numLength
+    var aArr = a.toString().split('')
+    var bArr = b.toString().split('')
+    var newArr = []
+    if(aArr.length >= bArr.length){
+        numLength = aArr.length
+        for(var i=0; i<numLength - bArr.length; i++){
+            bArr.unshift('0')
+        }
+    }
+    else {
+        numLength = bArr.length
+        for(var i=0; i<numLength - aArr.length; i++){
+            aArr.unshift('0')
+        }
+    }
+//     console.log(aArr)
+    for(var i=0; i<numLength; i++){
+        newArr[i] = Number(aArr[i]) + Number(bArr[i])
+    }
+//     console.log(newArr[1])
+    newArr.map(function(e, i){
+        if(i > 0 && e >= 10){
+            newArr[i - 1] = newArr[i - 1] + 1
+        }
+    })
+//     console.log(newArr)
+    newArr.map(function(e, i){
+        if(i > 0 && e >= 10){
+            newArr[i] = newArr[i].toString()[1]
+        }
+    })
+    return newArr.join('')
 }
+
 
 module.exports = add;
